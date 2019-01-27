6d2eb9e61636a38b0b7e8d60fbb2658de1b45bd3
Tomoki-K <tomofuma@gmail.com>
2019-01-27 18:12:43 +0900

    dynamic fontsize

--- src/components/Molecules/TextOnImage/TextOnImage.tsx
+++ src/components/Molecules/TextOnImage/TextOnImage.tsx
@@ -8,27 +8,53 @@ interface TextOnImageProps {
   className?: string;
 }
 
-export const TextOnImage: React.StatelessComponent<TextOnImageProps> = (props) => {
-  const { text, title, filename, className } = props;
+interface TextOnImageStates {
+  fontSize: number;
+}
 
-  // dynamic font size, based on parent div width. (default: 50px)
-  const fontSize = this.container ? this.container.offsetWidth * 0.07 : 50;
+export class TextOnImage extends React.Component<TextOnImageProps, TextOnImageStates> {
+  private ImageContainer = React.createRef<HTMLDivElement>();
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontSize: 0,
+    };
+  }
 
-  return(
-    <div className={`TextOnImage ${className}Image`}>
-      <p
-        className={`TextOnImageText ${className}Text`}
-        style={{ fontSize }}
-      >
-      {text}
-      </p>
-      <div ref={(container) => { this.container = container; }}>
-        <Image
-          title={title}
-          filename={filename}
-          className={`TextOnImageImage ${className}Image`}
-        />
+  public componentDidMount() {
+    this.setFontSize(); // set font size on mount
+    window.addEventListener('resize', this.setFontSize);
+  }
+  public componentWillUnmount() {
+    window.removeEventListener('resize', this.setFontSize);
+  }
+
+  public render() {
+    const { text, title, filename, className } = this.props;
+
+    return(
+      <div className={`TextOnImage ${className}Image`}>
+        <p
+          className={`TextOnImageText ${className}Text`}
+          style={{ fontSize: this.state.fontSize }}
+        >
+        {text}
+        </p>
+        <div ref={this.ImageContainer}>
+          <Image
+            title={title}
+            filename={filename}
+            className={`TextOnImageImage ${className}Image`}
+          />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+
+  private setFontSize = () => {
+    // dynamic font size, based on parent div width. (default: 50px)
+    const container = this.ImageContainer;
+    const fontSize = container!.current ? container.current.offsetWidth * 0.07 : 50;
+    this.setState({ fontSize });
+  }
+}
