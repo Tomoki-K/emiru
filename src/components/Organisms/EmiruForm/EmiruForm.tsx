f012035bfd0f34356cf55920b55b2c0553b31c4b
Tomoki-K <tomofuma@gmail.com>
2019-01-30 17:58:11 +0900

    download button works

--- src/components/Organisms/EmiruForm/EmiruForm.tsx
+++ src/components/Organisms/EmiruForm/EmiruForm.tsx
@@ -5,6 +5,8 @@ import { Button } from '../../Atoms/Button';
 import { TextArea } from '../../Atoms/TextArea';
 import { TextOnImage } from '../../Molecules/TextOnImage';
 
+import { ImagePayload } from '../../../payloads/ImagePayload';
+
 import routePaths from '../../../Routes/routePaths';
 
 type Status = 'STANDBY' | 'SENDING' | 'DOWNLOAD_SUCCESS' | 'FAILURE';
@@ -28,7 +30,7 @@ export class EmiruForm extends React.Component<EmiruFormProps, EmiruFormState> {
   constructor(props) {
     super(props);
     this.state = {
-      text: 'ギュイーンとソウルが\nシャウトするのです',
+      text: '',
       status: 'STANDBY',
     };
   }
@@ -37,18 +39,26 @@ export class EmiruForm extends React.Component<EmiruFormProps, EmiruFormState> {
     this.setState({ text: e.currentTarget.value });
   }
 
+  public saveBase64Image = (base64Data: string, name: string) => {
+    const link = document.createElement('a');
+    link.download = name;
+    link.href = base64Data.replace('image/png', 'image/octet-stream');
+    link.click();
+  }
+
   public handleDownload = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     if (this.state.text.length === 0) { return; }
     this.setState({ status: 'SENDING' });
-    axios.get(
+    axios.get<ImagePayload>(
       routePaths.API_IMAGE, {
         params: {
           text: this.state.text,
         },
       },
-    ).then((data) => {
+    ).then(({ data }) => {
       this.setState({ status: 'DOWNLOAD_SUCCESS' });
+      this.saveBase64Image(data.image, `emiru_${Date.now()}.png`);
     }).catch((err) => {
       this.setState({ status: 'FAILURE' });
     });
@@ -59,7 +69,7 @@ export class EmiruForm extends React.Component<EmiruFormProps, EmiruFormState> {
     return (
       <>
         <TextOnImage
-          text={this.state.text}
+          text={this.state.text || 'ギュイーンとソウルが\nシャウトするのです'}
           title="emiru_bg"
           filename="emiru.jpeg"
           className="EmiruFormPreview"
